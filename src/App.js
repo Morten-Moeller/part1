54b0ead9a3830ee86e1e727c8362a675d8bb7b89
Morten Möller <git@mortenm.de>
2021-05-12 23:25:00 +0200

    Complete exercise 1.3

--- src/App.js
+++ src/App.js
@@ -2,29 +2,24 @@ import React from 'react'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10,
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7,
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14,
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
-      <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total part1={part1} part2={part2} part3={part3} />
     </div>
   )
 
@@ -32,34 +27,32 @@ const App = () => {
     return <h1>{course}</h1>
   }
 
-  function Content({
-    part1,
-    part2,
-    part3,
-    exercises1,
-    exercises2,
-    exercises3,
-  }) {
+  function Content({ part1, part2, part3 }) {
     return (
       <>
         {' '}
-        <Part part={part1} exercises={exercises1} />
-        <Part part={part2} exercises={exercises2} />
-        <Part part={part3} exercises={exercises3} />
+        <Part name={part1.name} exercises={part1.exercises} />
+        <Part name={part2.name} exercises={part2.exercises} />
+        <Part name={part3.name} exercises={part3.exercises} />
       </>
     )
   }
 
-  function Part({ part, exercises }) {
+  function Part({ name, exercises }) {
     return (
       <p>
-        {part} {exercises}
+        {name} {exercises}
       </p>
     )
   }
 
-  function Total({ exercises1, exercises2, exercises3 }) {
-    return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+  function Total({ part1, part2, part3 }) {
+    return (
+      <p>
+        Number of exercises{' '}
+        {part1.exercises + part2.exercises + part3.exercises}
+      </p>
+    )
   }
 }
 
