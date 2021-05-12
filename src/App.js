da7a40f4ab6df3eba3bd01e9ee4965ce3ed76520
Morten Möller <git@mortenm.de>
2021-05-13 00:38:06 +0200

    Complete exercise 1.11 unicafe

--- src/App.js
+++ src/App.js
@@ -1,61 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-    },
-  ]
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const sum = good + neutral + bad
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <h2>give feedback</h2>
+      <button onClick={() => handleClickGood()}>good</button>
+      <button onClick={() => handleClickNeutral()}>neutral</button>
+      <button onClick={() => handleClickBad()}>bad</button>
+      <Statistics />
     </div>
   )
 
-  function Header({ course }) {
-    return <h1>{course}</h1>
-  }
+  function Statistics() {
+    const initial =
+      sum === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <table>
+          <tr>
+            <td>good</td>
+            <td>{good}</td>
+          </tr>
+          <tr>
+            <td>neutral</td>
+            <td>{neutral}</td>
+          </tr>
+          <tr>
+            <td>bad</td>
+            <td>{bad}</td>
+          </tr>
+          <tr>
+            <td>all</td>
+            <td>{sum}</td>
+          </tr>
+          <tr>
+            <td>average</td>
+            <td>{(good - bad) / sum}</td>
+          </tr>
+          <tr>
+            <td>tdositive</td>
+            <td>{(good / sum) * 100}%</td>
+          </tr>
+        </table>
+      )
 
-  function Content({ parts }) {
     return (
       <>
-        {' '}
-        <Part name={parts[0].name} exercises={parts[0].exercises} />
-        <Part name={parts[1].name} exercises={parts[1].exercises} />
-        <Part name={parts[2].name} exercises={parts[2].exercises} />
+        <h2>statistic</h2>
+        {initial}
       </>
     )
   }
 
-  function Part({ name, exercises }) {
-    return (
-      <p>
-        {name} {exercises}
-      </p>
-    )
+  function handleClickGood() {
+    setGood(good + 1)
   }
-
-  function Total({ parts }) {
-    console.log(parts)
-    return (
-      <p>
-        Number of exercises{' '}
-        {parts[0].exercises + parts[1].exercises + parts[2].exercises}
-      </p>
-    )
+  function handleClickNeutral() {
+    setNeutral(neutral + 1)
+  }
+  function handleClickBad() {
+    setBad(bad + 1)
   }
 }
 
